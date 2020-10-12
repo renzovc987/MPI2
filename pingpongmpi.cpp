0736aed198d5d6affe477e5fa02c6a326833614b
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-10-12 18:14:13 -0500

    Update pingpongmpi.cpp

--- pingpongmpi.cpp
+++ pingpongmpi.cpp
@@ -3,30 +3,30 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  const int PING_PONG_LIMIT = 10;
+  const int limite = 10;
   MPI_Init(NULL, NULL);
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  if (world_size != 2) {
-    fprintf(stderr, "El tamaño debe ser 2 para: %s\n", argv[0]);
+  int my_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  int my_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &my_size);
+  if (my_size != 3) {
+    fprintf(stderr, "El tamaño debe ser 3 para: %s\n", argv[0]);
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  int ping_pong_count = 0;
-  int partner_rank = (world_rank + 1) % 2;
-  while (ping_pong_count < PING_PONG_LIMIT) {
-    if (world_rank == ping_pong_count % 2) {
-      ping_pong_count++;
-      MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+  int contador = 0;
+  int partner_rank = (my_rank + 1) % 2;
+  while (contador < limite) {
+    if (my_rank == contador % 2) {
+      MPI_Send(&contador, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
       printf("%d Contador enviado e incrementado %d a %d\n",
-             world_rank, ping_pong_count, partner_rank);
+             my_rank, contador, partner_rank);
     } else {
-      MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+      MPI_Recv(&contador, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
+      contador++;
       printf("%d Contador recibido %d desde %d\n",
-             world_rank, ping_pong_count, partner_rank);
+             my_rank, contador, partner_rank);
     }
   }
   MPI_Finalize();
