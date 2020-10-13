af0ee80f9d875c44c49ef1fa2968e17e0e26a5fd
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-10-13 02:51:32 -0500

    Update trap.cpp

--- trap.cpp
+++ trap.cpp
@@ -1,39 +1,57 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+double f(double x) {
+   return x*x;
+} 
+double Trap(double left_endpt ,double right_endpt ,int trap_count,double base_len) {
+	double estimate, x;
+ 	int i;
+ 	estimate = (f(left_endpt) + f(right_endpt))/2.0;
+ 	for (i = 1; i <= trap_count-1; i++) {
+		 x = left_endpt + i*base_len;
+		 estimate += f(x);
+ 	}
+ 	estimate = estimate*base_len;
 
-int main(int argc, char** argv) {
-  //Se inicializa el entorno MPI
-  const int limite = 10;
-  MPI_Init(NULL, NULL);
-    //Se define my_rank y my_size  que serán los ids y el número de procesos
+ 	return estimate;
+ } 
+int main(void){
+	 int my_rank, comm_sz, n = 1024, local_n;
+	 double a = 0.0, b = 3.0, h, local_a, local_b;
+	 double local_int, total_int;
+	 int source;
 
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  int my_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &my_size);
-  //Se emplean paramatros que se deben cumplir en este caso el numero de procesos no debe ser mayor a 3
-  if (my_size != 3) {
-    fprintf(stderr, "El tamaño debe ser 3 para: %s\n", argv[0]);
-    MPI_Abort(MPI_COMM_WORLD, 1);
-  }
-  // Posteriormente se realizan las operaciones de envío y recibimiento utilizando las instrucciones
-  // MPI_Send y MPI_Recv
-  int contador = 0;
-  int rank_p = (my_rank + 1) % 3;
-  while (contador < limite) {
-    if (my_rank == contador % 3) {
-      contador = 0;
-      MPI_Send(&contador, 1, MPI_INT, rank_p, 0, MPI_COMM_WORLD);
-      printf("%d Contador enviado e incrementado %d a %d\n",
-             my_rank, contador, rank_p);
-    } else {
-      contador++;
-      MPI_Recv(&contador, 1, MPI_INT, rank_p, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      printf("%d Contador recibido %d desde %d\n",
-             my_rank, contador, rank_p);
-    }
-  }
-  MPI_Finalize();
+	 MPI_Init(NULL, NULL);
+	 MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	 MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+	 h = (b-a)/n; 
+	 local_n = n/comm_sz; 
+
+	 local_a = a + my_rank*local_n*h;
+	 local_b = local_a + local_n*h;
+	 local_int = Trap(local_a, local_b, local_n, h);
+
+	 if (my_rank != 0) {
+		 MPI_Send(&local_int, 1, MPI_DOUBLE, 0, 0,
+		 	MPI_COMM_WORLD);
+	 } 
+	 else {
+		 total_int = local_int;
+		 for (source = 1; source < comm_sz; source++) {
+			 MPI_Recv(&local_int, 1, MPI_DOUBLE, source, 0,
+			 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			 total_int += local_int;
+	 	}
+	 }
+
+	 if (my_rank == 0) {
+		 printf("With n = %d trapezoids, our estimate\n", n);
+		 printf("of the integral from %f to %f = %.15e\n",
+		  a, b, total_int);
+	 }
+	 MPI_Finalize();
+	 return 0;
 }
+
